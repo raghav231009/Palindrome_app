a01f61ad912d3a940d05f4198910063a02f50ba3
raghav231009 <sharmaraghav0911@gmail.com>
2026-02-24 16:37:50 +0530

    done usecase 4.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
-public class PalindromeMain {
+public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String word = "level";
+
+        char[] letters = word.toCharArray();
+
+        int start = 0;
+        int end = letters.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (letters[start] != letters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It IS a palindrome.");
+        } else {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It is NOT a palindrome.");
+        }
     }
 }
