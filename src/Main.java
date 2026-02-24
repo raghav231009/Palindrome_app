e7421a60b1ee307e49d304e9e9a87eaa84a4f6ab
raghav231009 <sharmaraghav0911@gmail.com>
2026-02-24 16:02:44 +0530

    Goal: Demonstrate FIFO vs LIFO using Queue and Stack.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
-public class PalindromeMain {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main{
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
