09a00bb599aca5b7fbee297d2bbe7314303796c0
raghav231009 <sharmaraghav0911@gmail.com>
2026-02-24 16:35:29 +0530

    done usecase 5.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-public class PalindromeMain {
+import java.util.Stack;
+
+public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
